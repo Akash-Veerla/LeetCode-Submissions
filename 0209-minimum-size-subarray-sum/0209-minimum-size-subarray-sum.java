d3ebd6da43b57756f96f790ef5b67ddeecdfce89
Akash Veerla <131761389+Akash-Veerla@users.noreply.github.com>
2025-07-07 21:02:59 +0530

    Time: 1 ms (99.65%), Space: 54.8 MB (99.02%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -11,15 +11,3 @@ class Solution {
         return (ans == Integer.MAX_VALUE) ? 0 : ans;
     }
 }
-
-/*
-int left = 0, currentSum = 0, minLength = Integer.MAX_VALUE;
-for (int right = 0; right < nums.length; right++) {
-    currentSum += nums[right];
-    while (currentSum >= target) {
-        minLength = Math.min(minLength, right - left + 1);
-        currentSum -= nums[left++];
-    }
-}
-return (minLength == Integer.MAX_VALUE) ? 0 : minLength;
-*/
