c69d5f574972aed9b6a334bd1e11df81df6ac733
Akash Veerla <131761389+Akash-Veerla@users.noreply.github.com>
2026-01-04 17:16:48 +0530

    Time: 0 ms (100%), Space: 43 MB (16.64%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -2,13 +2,11 @@ class Solution {
     public int rob(int[] nums) {
         int prevRob = 0;
         int maxRob = 0;
-
         for (int curValue : nums) {
             int temp = Math.max(maxRob, prevRob + curValue);
             prevRob = maxRob;
             maxRob = temp;
         }
-
         return maxRob;
     }
 }
